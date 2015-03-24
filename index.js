3652e95a0012e69938d723244f35f9137857ff92
Leigh Zhu <i@zhuli.me>
2015-03-24 15:49:50 +0800

    add cli argv parser

--- index.js
+++ index.js
@@ -4,8 +4,33 @@ var path = require('path');
 var walk = require('walk');
 var UPYUN = require('upyun');
 var mime = require('mime');
+var argv = require('minimist')(process.argv.slice(2));
 
-var config = require(path.resolve(process.cwd(), './config.js'));
+var config = {};
+if (typeof argv.config == 'string') {
+  config = JSON.parse(fs.readFileSync(argv.config))
+}
+
+if (argv.localPath) {
+  config.localPath = argv.localPath;
+}
+
+if (argv.baseDir) {
+  config.baseDir = argv.baseDir;
+}
+
+if (argv.bucket) {
+  config.bucket = argv.bucket;
+}
+
+if (argv.operator) {
+  config.operator = argv.operator;
+}
+
+if (argv.password) {
+  config.password = argv.password;
+}
+
 var walker = walk.walk(config.localPath);
 var _now = new Date().getTime();
 var upyun = new UPYUN(config.bucket, config.operator, config.password, 'ctcc', 'legacy');
